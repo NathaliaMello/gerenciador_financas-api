ea3c5f90884de36c2d41a9eb6a4bdb5971d1e954
Nathalia Mello <nathalia.mello@tty2000.com.br>
2021-01-22 23:09:48 -0300

    first commit

---
+++ src/main/java/com/nathaliamello/gerenciadorfinancasapi/GerenciadorFinancasApiApplication.java
@@ -0,0 +1,13 @@
+package com.nathaliamello.gerenciadorfinancasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorFinancasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorFinancasApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathaliamello/gerenciadorfinancasapi/GerenciadorFinancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathaliamello.gerenciadorfinancasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorFinancasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
